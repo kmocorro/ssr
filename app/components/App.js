5b7637efe73d80388f781551ed06b159da3ec96e
kmocorro <kevinmocorro.github@gmail.com>
2019-05-08 15:37:57 +0800

    wewe

--- app/components/App.js
+++ app/components/App.js
@@ -2,15 +2,35 @@ import React from 'react'
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 
 import Home from './Home'
+import Auth from './Auth'
 import RMP from './RMP'
 
 export default () => {
+    function isLoggedIn(){
+        let tokener = getToken();
+        return tokener; //blnk false
+    }
+
+    function getToken(){
+        return localStorage.getItem('ldap_token');
+    }
+
+
     return (
         <div>
-            <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/uploader/rmp" component={RMP}/>
-            </Switch>
+            { isLoggedIn()
+                ? 
+                <div>
+                    <Switch>
+                        <Route path="/" exact component={Home}/>
+                        <Route path="/uploader/rmp" component={RMP}/>
+                    </Switch>
+                </div>
+                : 
+                <div>
+                <Auth />
+                </div>
+            }
         </div>
     )
 }
