a0c2c24e90ce6524645b53a782fdbafc464cccee
kmocorro <kevinmocorro.github@gmail.com>
2019-05-09 07:04:40 +0800

    awe

--- app/components/DashboardBody.js
+++ app/components/DashboardBody.js
@@ -1,21 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReportsBody from './ReportsBody'
 
 export default (metaDashboard) => {
 
     const metaDashboard_props = metaDashboard.metaDashboard.meta.data;
     
-    //const medianEfficiency = metaDashboard_props.dashboard[0].value;
+    const medianEfficiency = metaDashboard_props.dashboard[0].value;
     const binNE = metaDashboard_props.dashboard[1].value;
     const cosmetics = metaDashboard_props.dashboard[2].value;
     const cycletime = metaDashboard_props.dashboard[3].value;
     
-
-    const [ medianEfficiency, setMedianEfficiency ] = useState(0);
-    
-    useEffect(() => {
-        setMedianEfficiency(metaDashboard_props);
-    }, [setMedianEfficiency]);
 
     return (
         <div style={{
