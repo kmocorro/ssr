3696ae0a51c2d7c2cd5c66ce589f3371dd15ff29
kmocorro <kevinmocorro.github@gmail.com>
2019-05-08 15:25:02 +0800

    aweaweaweawe

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
                 query: {
                     presets: ['es2015', 'react']
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
             }
         ]
     }
