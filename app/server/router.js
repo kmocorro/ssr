95995482f91a5d3b3b7a58673e985ab6a40158d5
kmocorro <kevinmocorro.github@gmail.com>
2019-05-09 14:15:14 +0800

    aweawe

--- app/server/router.js
+++ app/server/router.js
@@ -4,10 +4,8 @@ import { matchPath, StaticRouter } from 'react-router-dom'
 
 import routes from './routes'
 import renderFullPage from './renderFullPage'
-//import getDashboard from '../services/getDashboard'
+import getDashboard from '../services/getDashboard'
 import App from '../components/App'
-
-import axios from 'axios'
 
 import Cookies from 'universal-cookie'
 
@@ -29,45 +27,25 @@ export default function router(req, res){
 
     console.log(cookie_ldap);
 
-    function getDashboard(){
-        return axios.get(`http://dev-metaspf401.sunpowercorp.com:8080/api/dashboard`, {withCredentials: true}).then(response => {
-            const meta_api = { data: response.data }
-
-            console.log(meta_api);
-    
-            if(meta_api.data.code === 1){
-    
-                const context = {}
-                console.log(meta_api);
-                
-                const html = renderToString(
-                    <StaticRouter context={context} location={req.url} >
-                        <App meta={meta_api}/>
-                    </StaticRouter>
-                )
-        
-                res.status(200).send(renderFullPage(html, meta_api));
-    
-            } else {
-    
-                const context = {}
-                
-                const html = renderToString(
-                    <StaticRouter context={context} location={req.url} >
-                        <App meta={meta_api}/>
-                    </StaticRouter>
-                )
-    
-                res.status(200).send(renderFullPage(html, meta_api));
-            }
-        })
-        .catch(err => res.status(404).send(`${err}: gg sir.`));
-    }
-
     if(cookie_ldap){
         console.log('okay loggedin. there\'s token. now what? ');
-        
-        getDashboard();
+        return getDashboard()
+        .then(response => {
+            const meta_api = { data: response.data }
+            
+            const context = {}
+            console.log(meta_api);
+            
+            const html = renderToString(
+                <StaticRouter context={context} location={req.url} >
+                    <App meta={meta_api}/>
+                </StaticRouter>
+            )
+    
+            res.status(200).send(renderFullPage(html, meta_api));
+
+        })
+        .catch(err => res.status(404).send(`${err}: gg sir.`));
         
     } else {
         console.log('Cant read token. what\'s happening');
