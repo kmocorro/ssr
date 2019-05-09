c1e92ef49f3304037845b0e666d130fabf997291
kmocorro <kevinmocorro.github@gmail.com>
2019-05-09 14:18:47 +0800

    awe

--- app/components/App.js
+++ app/components/App.js
@@ -1,26 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 
+import Cookies from 'universal-cookie'
+
 import Home from './Home'
 import Auth from './Auth'
 import RMP from './RMP'
 import Login from './Login'
 
 export default (metaDashboard) => {
-    function isLoggedIn(){
-        let tokener = getToken();
-        return tokener; //blnk false
-    }
 
-    function getToken(){
-        if (typeof window !== 'undefined') {
-            return localStorage.getItem('ldap_token');
-        }
-    }
+    const cookies = new Cookies();
+    const cookie_ldap = cookies.get('ldap_token');
 
     return (
         <div>
-            { isLoggedIn()
+            { cookie_ldap
                 ? 
                 <div>
                 <Switch>
