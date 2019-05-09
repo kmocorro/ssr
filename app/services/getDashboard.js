4479aa53a9d0c0cf7886af848bb935baf28aedeb
kmocorro <kevinmocorro.github@gmail.com>
2019-05-09 13:40:03 +0800

    aw

--- app/services/getDashboard.js
+++ app/services/getDashboard.js
@@ -3,9 +3,8 @@ import axios from 'axios'
 export default () => {
     axios.defaults.withCredentials = true;
 
-    return axios('http://dev-metaspf401.sunpowercorp.com:8080/api/dashboard', {
-        method: 'GET',
-        withCredentials: true
-    });
-    
+    return axios.get(`http://dev-metaspf401.sunpowercorp.com:8080/api/dashboard`, 
+        {withCredentials: true}
+    );
+
 }
