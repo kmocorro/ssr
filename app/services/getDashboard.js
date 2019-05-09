59370b2a8e764828a805a23d2ff0809d1bee56ce
kmocorro <kevinmocorro.github@gmail.com>
2019-05-09 13:08:51 +0800

    withcred

--- app/services/getDashboard.js
+++ app/services/getDashboard.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default () => {
     const baseUrl = 'http://dev-metaspf401.sunpowercorp.com:8080/api/dashboard';
-    return axios.get(`${baseUrl}`)
+    return axios.get(`${baseUrl}`, {withCredentials: true})
 }
