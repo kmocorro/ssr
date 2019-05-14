6e00f71020631b6ef604e68716fa7c11cc845b39
kmocorro <kevinmocorro.github@gmail.com>
2019-05-14 13:40:43 +0800

    waeuoajlwkeaw

--- app/components/Auth.js
+++ app/components/Auth.js
@@ -167,7 +167,22 @@ export default () => {
                             <div>    
                             <small>Password</small>
                                 <div>
-                                    <input type="password" required value={password.value} onChange={password.onChange}  onClick={handleOnClick} />
+                                    <input type="password" required value={password.value} onChange={password.onChange}  onClick={handleOnClick} 
+                                        style={{
+                                            display: "block",
+                                            margin: "0",
+                                            padding: "0 1em 0",
+                                            border: "2px solid #fff ",
+                                            borderBottom: "1px solid #eb5424",
+                                            WebkitAppearance: "none",
+                                            boxSizing: "border-box",
+                                            width: "100%", height: "65px",
+                                            fontSize:"20px",
+                                            fontWeight: "400",
+                                            fontFamily: "inherit",
+                                            outlineStyle: "none",
+                                            transition: "box-shadow .2s linear, border-color .25s ease-out",
+                                        }} />
                                 </div>
                             </div>
 
