b88776eb5339ea579fada3a33c20aa87ad571d72
kmocorro <kevinmocorro.github@gmail.com>
2019-05-17 08:14:08 +0800

    8081

--- app/services/getRMP.js
+++ app/services/getRMP.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default (token) => {
-    return axios.get(`http://dev-metaspf401.sunpowercorp.com:8080/api/rmp/${token}`, {withCredentials: true});
+    return axios.get(`http://dev-metaspf401.sunpowercorp.com:8081/api/rmp/${token}`, {withCredentials: true});
 }
